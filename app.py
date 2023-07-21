7c8592a0021b52cad1b9fd0ea366d9fa1cb6ac6e
Kah Hee <kahhee.dev@hotmail.com>
2023-07-21 18:18:57 +0800

    fixed syntax error, added timestamp to personal info

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import mysql.connector
 import zipfile
 import json
 import os
+from datetime import datetime 
 
 # TODO:UPDATE BUSINESS OF NATURE
 # TODO:UPDATE TYPE OF OCCUPATION
@@ -45,23 +46,19 @@ class MyApp(Flask):
     def submitFirstPage(self):
         try:
             self.firstPageData = dict(request.form)
-            print("First Page Dictionary",self.firstPageData, flush=True)
-            print("NRIC :", self.firstPageData['NRIC'], flush=True)
             self.firstPageData['gender'] = 'Male'
             if int( self.firstPageData['NRIC']) % 2 == 0:
                 self.firstPageData['gender'] = 'Female'
             self.restructureFirstPageInfo()
+            print("Submitted first page . . .", flush=True)
             return redirect(url_for('page2'))
         except Exception as e:
-            print(e)
+            print(e, flush=True)
 
     # Submit Second Page Handler
     def submit(self):
-        print("From Second page (DATA): ", self.firstPageData, flush=True)
-        print("From Second page (NRIC): ", self.firstPageData['NRIC'], flush=True)
-        pdfFiles = request.files.getlist('pdfFiles')
-        zipFileName = f"./pdfFiles/{self.firstPageData['NRIC']}.zip"
-
+        print("Submitting form. . .", flush=True)
+        try:
         # try:
         #     capturedPhoto = json.loads(request.form.get('capturedPhoto'))
 
@@ -75,28 +72,35 @@ class MyApp(Flask):
         # except Exception as e:
         #     print(e)
         #     pass
+            pdfFiles = request.files.getlist('pdfFiles')
+            zipFileName = f"./pdfFiles/{self.firstPageData['NRIC']}.zip"
+            print("PDF Files", pdfFiles, flush=True)
 
+            merger = PdfMerger()
 
-        merger = PdfMerger()
+            for file in pdfFiles:
+                # check if the file has any content to avoid merging empty file
+                if file.content_length > 0:
+                    merger.append(file)
 
-        for file in pdfFiles:
-            # check if the file has any content to avoid merging empty file
-            if file.content_length > 0:
-                merger.append(file)
+            mergedPdfPath = f"./pdfFiles/{self.firstPageData['NRIC']}.pdf"
+            merger.write(mergedPdfPath)
+            merger.close()
 
-        mergedPdfPath = f"./pdfFiles/{self.firstPageData['NRIC']}.pdf"
-        merger.write(mergedPdfPath)
-        merger.close()
+            with zipfile.ZipFile(zipFileName, 'w') as zf:
+                zf.write(mergedPdfPath, f"{self.firstPageData['NRIC']}.pdf")
 
-        with zipfile.ZipFile(zipFileName, 'w') as zf:
-            zf.write(mergedPdfPath, f"{self.firstPageData['NRIC']}.pdf")
+            os.system('rm -rf ./pdfFiles/*.pdf')
 
-        os.system('rm -rf ./pdfFiles/*.pdf')
+            self.secondPageData = dict(request.form)
+            self.restructureSecondPageInfo()
 
-        self.secondPageData = dict(request.form)
-        self.restructureSecondPageInfo()
-
-        flag1 = flag2 = flag3 = flag4 = flag5 = flag6 = False
+            flag1 = flag2 = flag3 = flag4 = flag5 = flag6 = False
+        except Exception as e:
+            print("Error before sql statements", flush=True)
+            print(e,flush=True)
+            return f"<h1>{e}<h1>"
+            
         try:
             query = f"INSERT INTO `Personal Info` VALUES ({self.personalInfo})"
             self.cursor.execute(query)
@@ -151,6 +155,7 @@ class MyApp(Flask):
             try: 
                 self.db.commit()
                 print("COMMITED INTO DB", flush=True)
+                print("Submit complete", flush=True)
             except Exception as e:
                 print(e, flush=True)
                 return f"<h1>{e}<h1>"
@@ -161,63 +166,60 @@ class MyApp(Flask):
 
     # Restructure First Page Data into a string
     def restructureFirstPageInfo(self):
-        data = self.firstPageData
-        self.personalInfo = f"'{data['NRIC']}', '{data['name']}', '{data['countryCode'] + data['phoneNumber']}', '{data['email']}', '{data['title']}', '{data['gender']}', '{data['race'] if data['race'] != '' else data['otherRace']}', '{data['maritalStatus']}', '{data['bumiornon']}', '{data['address']}', '{data['numOfYear']}', '{data['ownership']}', '{data['stayRegisterAddress']}', '{data['noStayRegisterAddress'] if data['stayRegisterAddress'] == 'No' else 'None'}'"
-        self.productInfo = f"'{data['NRIC']}', '{data['productType'].lower()}{data['newusedrecon'].lower()}', '{data['brand']}', '{data['modal']}', '{data['usedNumberPlate'] if data['newusedrecon'] == 'Used' else data['reconNumberPlate'] if data['newusedrecon'] == 'Recon' else 'None'}', '{data['tenure']}'"
+        try:
+            data = self.firstPageData
+            
+            now = datetime.now()
+            currentTime = now.strftime("%Y-%m-%d %H:%M:%S")
+            
+            self.personalInfo = f"'{data['NRIC']}', '{data['name']}', '{data['countryCode'] + data['phoneNumber']}', '{data['email']}', '{data['title']}', '{data['gender']}', '{data['race'] if data['race'] != '' else data['otherRace']}', '{data['maritalStatus']}', '{data['bumiornon']}', '{data['address']}', '{data['numOfYear']}', '{data['ownership']}', '{data['stayRegisterAddress']}', '{data['noStayRegisterAddress'] if data['stayRegisterAddress'] == 'No' else 'None'}', '{currentTime}'"
+            self.productInfo = f"'{data['NRIC']}', '{data['productType'].lower()}{data['newusedrecon'].lower()}', '{data['brand']}', '{data['modal']}', '{data['usedNumberPlate'] if data['newusedrecon'] == 'Used' else data['reconNumberPlate'] if data['newusedrecon'] == 'Recon' else 'None'}', '{data['tenure']}'"
 
-        referenceContact1 = f"'{data['NRIC']}', '{data['referenceName1']}', '{data['referenceNric1']}', '{data['referenceCountryCode1'] + data['referencePhoneNum1']}', '{data['stayWithReference1']}', '{data['notStayWithApplicant1'] if data['stayWithReference1'] == 'No' else 'None'}', '{data['referenceRelation1']}'"
+            referenceContact1 = f"'{data['NRIC']}', '{data['referenceName1']}', '{data['referenceNric1']}', '{data['referenceCountryCode1'] + data['referencePhoneNum1']}', '{data['stayWithReference1']}', '{data['notStayWithApplicant1'] if data['stayWithReference1'] == 'No' else 'None'}', '{data['referenceRelation1']}'"
 
-        referenceContact2 = f"'{data['NRIC']}', '{data['referenceName2']}', '{data['referenceNric2']}','{data['referenceCountryCode2'] + data['referencePhoneNum2']}', '{data['stayWithReference2']}', '{data['notStayWithApplicant2'] if data['stayWithReference2'] == 'No' else 'None'}', '{data['referenceRelation2']}'"
+            referenceContact2 = f"'{data['NRIC']}', '{data['referenceName2']}', '{data['referenceNric2']}','{data['referenceCountryCode2'] + data['referencePhoneNum2']}', '{data['stayWithReference2']}', '{data['notStayWithApplicant2'] if data['stayWithReference2'] == 'No' else 'None'}', '{data['referenceRelation2']}'"
 
-        self.referenceContacts.clear()
-        self.referenceContacts.append(referenceContact1)
-        self.referenceContacts.append(referenceContact2)
-        print('DEBUGGING DATA: ',data, flush=True)
-        print('DEBUGGING Personal Info: ',self.personalInfo, flush=True)
-        print('DEBUGGING Product Info: ',self.productInfo, flush=True)
+            self.referenceContacts.clear()
+            self.referenceContacts.append(referenceContact1)
+            self.referenceContacts.append(referenceContact2)
+            print("Finish Restructure First Page Info", flush=True)
+        except Exception as e:
+            print("Error in Restructure First Page Info", flush=True)
+            print(e, flush=True)
+            return f"<h1>{e}<h1>"
 
     # Restructure Second Page Data into a string
     def restructureSecondPageInfo(self):
         try: 
             data = self.secondPageData
-            print("DEBUGGING SECOND PAGE DATA: ", data, flush=True)
             employmentStatus = data['employmentStatus']
             status = ''
             if employmentStatus == 'employed':
                 status = data['NemployedStatus']
-                print(status,flush=True)
             elif employmentStatus == 'other':
                 status = data['selfEmployedOther']
                 if status == 'yes':
                     status = data['otherIncomeSourceOther']
             elif employmentStatus == 'student':
                 status = 'None'
+                data['department'] = "None"
+                data['position'] = "None"
+                
             elif employmentStatus == 'self-employed' and data.get('selfEmployedStatus','') != 'other':
                 status = data['selfEmployedStatus']
             else:
                 status = data['selfEmployedOther']
-
-            if data['employmentStatus'] == 'student':
-                data['department'] = "None"
-                data['position'] = "None"
             
-            print("Debugging 1", flush=True)
             self.workingInfo = f"'{self.firstPageData['NRIC']}', '{data['employmentStatus']}', '{status}', '{data['position']}', '{data['department']}', '{data['businessNature']}', '{data['companyName']}', '{data['companyCountryCode'] + data['companyPhoneNumber']}', '{data['workinginsingapore']}', '{data['companyAddress']}', '{data['whenJoinedCompany']}', 'RM {data['netSalary'] + '.' + data['netSalaryDecimal']}', 'RM {data['grossSalary'] + '.' + data['grossSalaryDecimal']}', '{data['efpGross']}', '{data['salaryTerm']}'"
-            print("Debugging 2", flush=True)
 
-            print("Debugging 3", flush=True)
             self.bankingInfo = f"'{self.firstPageData['NRIC']}', '{data['bankName']}', '{data['bankAccountNumber']}', '{data['typeOfAccount'] if data['typeOfAccount'] != 'other' else data['typeOfAccountOther']}', './pdfFiles/{self.firstPageData['NRIC']}.zip'"
 
             self.extraInfo = f"'{self.firstPageData['NRIC']}', '{data['bestContactTime']}', '{data['motorLicense']}', '{data['licenseType'] if data['motorLicense'] == 'Yes' else 'None'}', '{data['howToKnowMotosing']}'"
-            print("Debugging 4", flush=True)
-
-
-            print("DEBUGGING Working Info: ", self.workingInfo, flush=True)
-            print("DEBUGGING Banking Info: ", self.bankingInfo, flush=True)
-            print("DEBUGGING Extra Info: ", self.extraInfo, flush=True)
-
+            print("Finish Restructure Second Page Info", flush=True)
         except Exception as e:
+            print("Error in Restructure Second Page Info", flush=True)
             print(e, flush = True)
+            return f"<h1>{e}<h1>"
 
 app = MyApp(__name__)
 if __name__ == '__main__':
